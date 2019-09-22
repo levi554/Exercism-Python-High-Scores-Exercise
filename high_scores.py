2e56cec397eeae0a385f4da46a898ee2d80bcf7a
Jonathan David Livesay <jonl316@gmail.com>
2019-09-21 22:01:02 -0500

    Added finished working on the personal_top_three to get the tests working

--- high_scores.py
+++ high_scores.py
@@ -1,10 +1,16 @@
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    sorted_scores = sorted(scores, reverse=True)
+    if (len(sorted_scores) == 2):
+        return [sorted_scores[0], sorted_scores[1]]
+    elif (len(sorted_scores) == 1):
+        return [sorted_scores[0]]
+    else:
+        return [sorted_scores[0], sorted_scores[1], sorted_scores[2]]
